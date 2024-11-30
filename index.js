e23aa1baa6eb39375449d6f29a1ecf72a6797806
Eric Bear <eric.lj.bear@gmail.com>
2024-11-29 18:28:54 -0600

    link JS file to EJS file

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// Home Page
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.render("index.ejs");
 })
 
 app.listen(port, () => {
